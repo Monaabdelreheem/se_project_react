65c3275a5df94b4b6958fde9a6bbd3a20395c8bb
Mona Abdelreheem <m.abdelraheem@aol.com>
2025-10-13 00:52:53 -0500

    Feat: render and filter cards + style card layout + add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+        <div className="footer__copyright">
+      <p className="footer__author">
+        Developed by Mona Abdelreheem 
+        </p>
+        <span className="footer__year" >2022</span>
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,17 +1,32 @@
+import "./Main.css";
 import WeatherCard from "./WeatherCard";
+import ItemCard from "./ItemCard";
+import { defaultClothingItems } from "../../utils/constants";
 
-function Main() {
+
+
+function Main({ weatherData }) {
   return (
     <main>
       <WeatherCard />
       <section className="cards">
-        <p className="cards__texts">
+        <p className="cards__text">
             Today is 75 &deg; F / You may want to wear:
         </p>
-        {/* TODO add the cards */}
+        <ul className="cards__list">
+          {defaultClothingItems
+        //   .filter((item) => {
+        //      return item.weather === weatherData.type;
+        //   })
+          .map((item) => {
+            return <ItemCard key={item._id} item={item} />;
+          })}   
+        </ul>
       </section>
     </main>
   );
 }
+
+
 
 export default Main;

70377995e299812513a3e5bb3e65219ef25ddb3e
Mona Abdelreheem <m.abdelraheem@aol.com>
2025-11-18 18:48:10 -0600

    feat: update WeatherCard to use temperature unit context for F/C display

--- src/components/Main/WeatherCard.jsx
+++ src/components/Main/WeatherCard.jsx
@@ -1,7 +1,11 @@
 import "./WeatherCard.css";
 import { weatherOptions } from "../../utils/constants";
+import { useContext } from "react";
+import currentTemperatureUnitContext from "../../contexts/currentTemperatureUnitContext";
 
 function WeatherCard({ weatherData }) {
+  const { currentTemperatureUnit } = useContext(currentTemperatureUnitContext);
+
   const filteredOptions = weatherOptions.filter((option) => {
     // normalize OpenWeather condition values so they match constants
     const cond = String(weatherData.condition || "").toLowerCase();
@@ -23,7 +27,7 @@ function WeatherCard({ weatherData }) {
 
   return (
     <section className="weather-card">
-      <p className="weather-card__temp">{weatherData.temp.F} &deg; F</p>
+      <p className="weather-card__temp">{currentTemperatureUnit === "F" ? weatherData.temp.F : weatherData.temp.C} &deg; {currentTemperatureUnit}</p>
       {weatherOption && (
         <img
           src={weatherOption.url}

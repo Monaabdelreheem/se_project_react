1a2d570a1c2e896c7bbb01cc8894139aefbef527
Mona Abdelreheem <m.abdelraheem@aol.com>
2025-11-20 19:07:26 -0600

    feat: add items API methods (getItems, addItem, removeItem)

---
+++ src/utils/api.js
@@ -0,0 +1,39 @@
+const baseUrl = "http://localhost:3001";
+
+const headers = { "Content-Type": "application/json" };
+
+export const getItems = () => {
+  return fetch(`${baseUrl}/items`, { headers }).then((res) => {
+    if (res.ok) {
+      return res.json();
+    } else {
+      return Promise.reject(`Error: ${res.status}`);
+    }
+  });
+};
+
+export const addItem = ({ name, imageUrl, weather }) => {
+  return fetch(`${baseUrl}/items`, {
+    method: "POST",
+    headers,
+    body: JSON.stringify({ name, imageUrl, weather }),
+  }).then((res) => {
+    if (res.ok) {
+      return res.json();
+    } else {
+      return Promise.reject(`Error: ${res.status}`);
+    }
+  });
+};
+export const removeItem = (itemId) => {
+  return fetch(`${baseUrl}/items/${itemId}`, {
+    method: "DELETE",
+    headers,
+  }).then((res) => {
+    if (res.ok) {
+      return res.json();
+    } else {
+      return Promise.reject(`Error: ${res.status}`);
+    }
+  });
+};
